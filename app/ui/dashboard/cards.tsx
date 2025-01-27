13efcd6461440107e81fc0772c0aa50501593cb2
Joel German <joelgerman671@gmail.com>
2025-01-27 14:27:12 -0400

    Add dashboard components with data fetching and loading states

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -15,7 +15,12 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-  const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers} = await fetchCardData();
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers,
+  } = await fetchCardData();
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
